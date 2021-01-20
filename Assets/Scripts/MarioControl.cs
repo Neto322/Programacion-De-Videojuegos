557a12a75c0f79b26468f51a0aebc141873886f3
Neto322 <42181276+Neto322@users.noreply.github.com>
2021-01-20 12:03:34 -0600

    Commit

---
+++ Assets/Scripts/MarioControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioControl : MonoBehaviour
+{
+
+    [SerializeField, Range(0, 10)]
+    float speed;
+
+    Animator anim;
+
+    SpriteRenderer spr;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Axis.x * speed * Time.deltaTime);
+
+        spr.flipX = Flip;
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+    }
+
+
+    Vector2 Axis 
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+    }
+
+
+    bool Flip
+    {
+        get => Axis.x < 0f ? false : Axis.x > 0f ? true : spr.flipX;
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+    
+}
